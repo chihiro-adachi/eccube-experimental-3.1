b5ae181482730e7431d81db903ba461145ca05e3
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2016-12-16 20:48:46 +0900

    コメント追加

--- EntityExtendSample/proxy.php
+++ EntityExtendSample/proxy.php
@@ -14,11 +14,19 @@ $conn = array(
 $generator = \Zend\Code\Generator\ClassGenerator::fromReflection(
     new \Zend\Code\Reflection\ClassReflection('Product')
 );
+
 $generator->setName('ProductProxy');
+// ProductクラスにTraitを追加する
 $generator->addTrait('PriceTrait');
 $generator->addTrait('TaxTrait');
 $code = $generator->generate();
+
+// プロキシを生成
 file_put_contents('src/ProductProxy.php', '<?php '.PHP_EOL.$code);
 
 $em = EntityManager::create($conn, $config);
+// プロキシを指定してfindする -> tax, priceが取得できる
 $em->find('ProductProxy', 1);
+
+// todo: Proxyを直接指定せず、裏で解決する
+// @see https://github.com/Ocramius/ProxyManager
